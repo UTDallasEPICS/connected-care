94873d197b4032e47a5fbd36ffca1bef51ca9d9d
Alison <168605300+akao335@users.noreply.github.com>
2025-10-23 12:03:22 -0500

    dashboard created

---
+++ pages/patientDashboard.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="font-karla">
+		<div class="flex min-h-[70vh] flex-col items-center justify-center">
+			<!--Image here -->
+			<img src="/manos-unidas-logo.jpg" alt="Logo" class="mb-10 w-45" />
+
+			<!-- Buttons -->
+			<div class="cols-4 grid grid-cols-4 gap-8 text-center sm:grid">
+				<!-- view schedule -->
+
+				<div class="flex flex-col items-center">
+					<button
+						@click="goTo('/scheduleView')"
+						class="flex h-32 w-32 flex-col items-center justify-center rounded-2xl bg-gray-300 p-6 transition hover:bg-gray-400"
+					></button>
+					<span class="text -sm font-medium">SCHEDULE</span>
+				</div>
+
+				<!-- view profile -->
+				<div class="flex flex-col items-center">
+					<button
+						@click="goTo('/patientProfile')"
+						class="flex h-32 w-32 flex-col items-center justify-center rounded-2xl bg-gray-300 p-6 transition hover:bg-gray-400"
+					></button>
+					<span class="text -sm font-medium">VIEW PROFILE</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { navigateTo, useCookie } from "#imports";
+import { AccessPermission } from "~/permissions";
+
+// Access cookies for authentication / role check
+const access = useCookie("AccessPermission");
+
+// Redirect if not admin
+if (access.value !== AccessPermission.PATIENT) {
+	navigateTo("/dashboard");
+}
+
+// Navigation helper
+function goTo(path: string) {
+	navigateTo(path);
+}
+</script>
