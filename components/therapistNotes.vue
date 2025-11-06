85c0bb9e2d1dbf0d3da3b646c437cae84c378830
hernandezclara <hernandezclara146@gmail.com>
2025-11-06 15:23:53 -0600

    updates on therapy notes implementations (#127)
    * updated "write progress report" button to "therapy notes"
    
    * removed progress notes features in order to create a drop down menu to view therapy notes
    
    * created a drop down menu for therapy session types
    
    * 110 spelling errors clara (#118)
    
    * fixed typos and changed session types from spanish to english
    
    * .create => .upsert for  typo and language changes
    
    * Calendar Wonky Scroll Fixed (#119)
    
    Fixed the calendar's weird scroll bar by erasing the calendar ui entirely and leaving it as basically a text field with calendar styling
    
    * Fixed Prisma config deprecation warning (#120)
    
    * feat: add therapistNotes component
    
    ---------
    
    Co-authored-by: Sanjay Bharathi <156241357+sanjay-b7@users.noreply.github.com>
    Co-authored-by: Neelesh Iyer <114715965+n8642i@users.noreply.github.com>
    Co-authored-by: veda-manchana <manchana.veda@gmail.com>

---
+++ components/therapistNotes.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="font-karla">
+		<div class="flex h-auto place-content-center">
+			<div
+				class="w-full max-w-5xl flex-col flex-wrap font-light md:w-180"
+			>
+				<h1 class="mt-5 ml-5 text-4xl sm:mr-5 sm:ml-0">
+					Therapist’s Notes
+				</h1>
+
+				<!-- content goes here next step -->
+				<div class="p-5 text-lg">
+					<div class="rounded-xl bg-white p-5 shadow">
+						<!-------- Session header (prefill later)----------->
+
+						User's full name: User's age: Date of birth: Age:
+						Diagnosis: Record number: Sex: Medication: Allergies:
+						Diet: Parent/Guardian's name:
+						<section>
+							<h2 class="mb-4 text-2xl font-semibold">
+								Session Information
+							</h2>
+							<div class="mb-6 grid grid-cols-1 gap-4"></div>
+						</section>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
