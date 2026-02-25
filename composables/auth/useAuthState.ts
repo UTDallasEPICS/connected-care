a0d0ecca76f4a3537f488f02667489d798b7d28b
PyroSh0ck <parthmodi.amodi@gmail.com>
2026-02-25 16:04:03 -0600

    First commit
    Fixed issues with login + different accounts. Issue with therapist accessing patients.

--- composables/auth/useAuthState.ts
+++ composables/auth/useAuthState.ts
@@ -1,14 +1,13 @@
+import { useLocalePath } from "#imports";
+import type { AccessVal } from "~/types/permissions";
+import type { CookieRef } from "#app";
 export function useAuthState() {
 	const userId = useCookie("userId");
 
 	// since for whatever reason access is stored as a JSON object,
 	// instead of parsing it each time, it just gets parsed as it comes out of the cookie
 
-	const access = useCookie("AccessPermission", {
-		default: () => null,
-		decode: (v) => (v ? JSON.parse(v) : null),
-		encode: (v) => JSON.stringify(v),
-	});
-
-	return { userId, access };
+	const access: CookieRef<AccessVal | null> = useCookie("AccessPermission");
+	const localePath = useLocalePath();
+	return { userId, access, localePath };
 }
